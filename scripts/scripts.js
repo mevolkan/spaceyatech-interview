e7c75564d2b9875c0ec5371be767fab976cce858
volkan <nzaro19@gmail.com>
2023-11-20 08:50:28 +0300

    cleanup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Function to fetch advice from the API
-  function fetchAdvice() {
-    fetch("https://api.adviceslip.com/advice")
-      .then((response) => response.json())
-      .then((data) => {
-        const advice = data.slip;
-        const adviceIdElement = document.getElementById("advice-number");
-        const adviceContentElement = document.getElementById("advice-content");
-
-        // Assigning advice ID and content to HTML elements
-        adviceIdElement.textContent = advice.id;
-        adviceContentElement.textContent = advice.advice;
-      })
-      .catch((error) => {
-        console.error("Error fetching advice:", error);
-      });
-  }
-
   // Initial call to fetch advice on page load
   fetchAdvice();
-
-  // Adding click event listener to fetch advice when the button is clicked
-  const fetchAdviceClick = document.getElementById("fetchAdvice");
-  fetchAdviceClick.addEventListener("click", fetchAdvice);
 });
+
+// Function to fetch advice from the API
+function fetchAdvice() {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((data) => {
+      const advice = data.slip;
+      const adviceIdElement = document.getElementById("advice-number");
+      const adviceContentElement = document.getElementById("advice-content");
+
+      // Assigning advice ID and content to HTML elements
+      adviceIdElement.textContent = advice.id;
+      adviceContentElement.textContent = advice.advice;
+    })
+    .catch((error) => {
+      console.error("Error fetching advice:", error);
+    });
+}
